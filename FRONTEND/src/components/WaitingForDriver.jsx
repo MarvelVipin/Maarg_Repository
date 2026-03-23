461c95a3cf1c64a25e5b034fdcd2dce4779e7bed
MarvelVipin <vipinvaranasi10@gmail.com>
2026-03-23 17:58:34 +0530

    forcing contribution

--- FRONTEND/src/components/WaitingForDriver.jsx
+++ FRONTEND/src/components/WaitingForDriver.jsx
@@ -4,7 +4,7 @@ const WaitingForDriver = () => {
   return (
     <div>
       <h5 className='p-1 text-center w-[93%] absolute top-0' onClick={()=>{props.setVehiclePanel(false)}}><i className="text-3xl text-gray-300 ri-arrow-down-wide-line pt-14"></i></h5>
-      <h3 className='text-2xl font-semibold mb-5'>Waiting for a driver</h3>
+      <h3 className='text-2xl font-semibold mb-5'>Waiting for a drivers...</h3>
 
       <div className='flex gap-2 justify-between flex-col items-center'>
         <img className='h-20' src="https://cn-geo1.uber.com/image-proc/crop/resizecrop/udam/format=auto/width=552/height=368/srcb64=aHR0cHM6Ly90Yi1zdGF0aWMudWJlci5jb20vcHJvZC91ZGFtLWFzc2V0cy85OWJmYWM5Mi00ODAzLTQxNGMtODRmYi1kMWZmNjU0NWM5YzAucG5n" alt="" />
