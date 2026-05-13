c0f1cc9adcc9e6bacad556dee00fcc3854e4cd01
MarvelVipin <vipinvaranasi10@gmail.com>
2026-05-13 11:01:55 +0530

    Deployment

--- BACKEND/db/db.js
+++ BACKEND/db/db.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 
-function connectToDb () {
-    mongoose.connect(process.env.DB_CONNECT).then (()=> {
-        console.log("Connected to the DB successfully");
-    }).catch((err)=> {
-        console.log("Error connecting to the DB", err);
-    });
-
+function connectToDb() {
+    mongoose.connect(process.env.MONGO_URI)
+        .then(() => {
+            console.log("Connected to the DB successfully");
+        })
+        .catch((err) => {
+            console.log("Error connecting to the DB", err);
+        });
 }
 
 module.exports = connectToDb;
